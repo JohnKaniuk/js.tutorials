7942ef9241ecf0b9c5cba423cbb1fa6103b3e8f4
JohnKaniuk <jaakkaan@gmail.com>
2020-05-27 14:28:23 -0400

    Todo-app hs been create

---
+++ .vs/toDo-app/toDo.js
@@ -0,0 +1,44 @@
+// const ps = document.querySelectorAll('p')
+
+// paragraphs.forEach(function(paragraph){
+//     if (paragraph.textContent.includes("the")){
+//         paragraph.remove()
+//     }
+// })
+const todos = [{
+    name: 'walk the dog',
+    comepleted: false
+   },{
+    name :'brush my teeth',
+    comepleted: false
+   },{
+    name : 'go on a walk',
+    comepleted: true
+   },{ 
+    name :'do coding',
+    comepleted: true 
+   },{
+    name : 'play some games',
+    comepleted: true
+   }]
+
+   // you have 2 todos left
+   // add a p for each todo 
+//    const newParagraph = document.createElement('p')
+//    newParagraph.textContent = forEach(todos.name)
+//    document.querySelector('body').appendChild
+   //    const ps = document.querySelector(todos)
+//    paragraphs.forEach(function(){})
+const incompleteTodos = todos.filter(function(todo){
+    return !todo.comepleted
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `you have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function(todos){
+    const p = document.createElement('p')
+    p.textContent= todos.text 
+    document.querySelector('body').appendChild(p)
+})
--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,12 +1,40 @@
+
 // DOM - document object model
 
 // const p= document.querySelector('p')
 // p.remove()
 //query all and remove
-const ps = document.querySelectorAll('p')
+//const ps = document.querySelectorAll('p')
 
-ps.forEach(function (p){
-   p.textContent= '**********'
+//ps.forEach(function (p){
+   //p.textContent= '**********'
     //console.log(p.textContent)
     //p.remove()
-})
+//})
+
+// add a new element
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from Javascript'
+// document.querySelector('body').appendChild(newParagraph)
+const todos = [{
+    name: 'walk the dog',
+    comepleted: false
+   },{
+    name :'brush my teeth',
+    comepleted: false
+   },{
+    name : 'go on a walk',
+    comepleted: true
+   },{ 
+    name :'do coding',
+    comepleted: true 
+   },{
+    name : 'play some games',
+    comepleted: true
+   }]
+
+   // you have 2 todos left
+   // add a p for each todo 
+   const newParagraph = document.createElement('p')
+   newParagraph.textContent = forEach(todos.name)
+   document.querySelector('body').appendChild
